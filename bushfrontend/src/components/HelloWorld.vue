7ab2c8921a6b98d86c4b4b566e5ba05d8c9f8d37
Карачев Кирилл <umbrellamixmail@gmail.com>
2024-07-29 23:22:05 +0700

    Основа проекта

---
+++ bushfrontend/src/components/HelloWorld.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <p>
+    <p>Здесь мог быть ваш сайт</p>
+  </p>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
---
+++ bushfrontend/vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  server: {
+    port: 8080,
+    proxy: {
+      "/api": {
+        //changeOrigin: true,
+        target: "http://127.0.0.1:8000",
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  },
+})
