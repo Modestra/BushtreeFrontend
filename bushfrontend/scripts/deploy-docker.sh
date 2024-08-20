98b2ff9b01d0b7f698de72fb6f43475abb10bdc4
Карачев Кирилл <umbrellamixmail@gmail.com>
2024-08-20 17:15:16 +0700

    Добавление скриптf

--- bushfrontend/scripts/deploy-docker.sh
+++
@@ -1,4 +0,0 @@
-#!/b in/bash/
-cd /usr/bashtreefrontend/bushfrontend
-sudo docker compose down
-sudo docker compose up --build -d
--- bushfrontend/scripts/docker-install.sh
+++ bushfrontend/scripts/docker-install.sh
@@ -1,1 +1,4 @@
-#/bin/bash
+#!/bin/bash/
+cd /usr/bashtreefrontend/bushfrontend
+sudo docker compose down
+sudo docker compose up --build -d
