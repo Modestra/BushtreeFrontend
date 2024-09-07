d6ef769e3de5e8a84b8029e44bacb83598ec17b9
someguy3021 <162592428+someguy3021@users.noreply.github.com>
2024-09-07 21:59:01 +0500

    WIP CORSs

--- bushfrontend/src/services/sessions.ts
+++ bushfrontend/src/services/sessions.ts
@@ -5,6 +5,9 @@ export function CreateSession(data) {
   return HTTP.post("/createseccion/", data, {
     headers: {
       "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "*",
+      "Access-Control-Allow-Headers": "*",
     },
   });
 }
@@ -14,6 +17,31 @@ export function GetSession() {
     headers: {
       "Content-Type": "application/json",
       "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "*",
+      "Access-Control-Allow-Headers": "*",
     },
   });
 }
+
+// export function GetSession(): Promise<any> {
+//   const headers = {
+//     "Content-Type": "application/json",
+//     "Access-Control-Allow-Origin": "*",
+//     "Access-Control-Allow-Methods": "*",
+//     "Access-Control-Allow-Headers": "*",
+//   };
+
+//   return HTTP.get("/seccions/list/", { headers });
+// }
+
+// export function GetSession() {
+//   return HTTP.get("/seccions/list/", {
+//     headers: {
+//       "Content-Type": "application/json",
+//       "Access-Control-Allow-Origin": "*",
+//       "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+//       "Access-Control-Allow-Headers":
+//         "Origin, X-Requested-With, Content-Type, Accept",
+//     },
+//   });
+// }
